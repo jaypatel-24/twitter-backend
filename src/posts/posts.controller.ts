b55c31ec82ff4bc311febd7a0ddf91fc5e8d09c1
Jay Patel <friend.jay24@gmail.com>
2021-07-09 10:13:19 +0530

    completed POSTS route in POSTS controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  @Get()
+  getAllPosts(): string {
+    return 'GET All posts';
+  }
+
+  @Get('/:postId')
+  getPostDetails(@Param() param): string {
+    return `get post with postId = ${param.postId}`;
+  }
+
+  @Post('')
+  createNewPost(): string {
+    return 'new post created';
+  }
+
+  @Delete('/:postId')
+  deletePost(@Param() param): string {
+    return `delete post with post id = ${param.postId}`;
+  }
+
+  @Put(':postId/like')
+  likePost(@Param() param): string {
+    return `like post with id = ${param.postId}`;
+  }
+
+  @Delete(':postId/like')
+  unlikePost(@Param() param): string {
+    return `unlike post with id = ${param.postId}`;
+  }
+}
